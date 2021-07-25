177288ec966372eedd07bc248fbcfa23d885dbd3
Hector Alvarez <halain80@gmail.com>
2021-07-24 21:53:12 -0500

    front, register new user

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +12,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  myForm: FormGroup = this.fb.group({
+    name: ['Test 1', [Validators.required]],
+    email: ['test1@test.com', [Validators.required, Validators.email]],
+    password: ['123456', [Validators.required, Validators.minLength(6)]]
+  });
+
+  constructor(private fb: FormBuilder, 
+              private router: Router, 
+              private authService: AuthService) { }
 
   ngOnInit(): void {
+  }
+
+  register() {
+    const {email, password, name} = this.myForm.value;
+    this.authService.register(name, email, password)
+      .subscribe( ok => { 
+        console.log(ok);
+               
+        if (ok === true) {
+          this.router.navigate(['/commits']);
+        } else {
+          Swal.fire({
+            icon: 'error',
+            title: 'Error...',
+            text: ok,
+            heightAuto: false,
+          })
+        }
+      });
   }
 
 }
---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,43 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+import { tap, map, catchError } from 'rxjs/operators';
+
+import { environment } from '../../../environments/environment';
+import { User, AuthResponse } from '../interfaces/auth.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private baseURL: string = environment.baseURL;
+
+  private _user!: User;
+
+  get user(){
+    return { ...this._user };
+  }
+
+  constructor(private http: HttpClient) { }
+
+  
+  register(name: string, email: string, password: string){
+
+    const url = `${this.baseURL}/auth/new`
+    const body = {email, password, name};
+
+    return this.http.post<AuthResponse>(url, body)
+      .pipe(
+        tap( ({ok, token}) => {
+            if (ok) {
+              localStorage.setItem('token', token!);
+            }
+        }),
+        map( resp => resp.ok),
+        catchError( err => of(err.error.msg) )
+      );
+  }
+
+
+}
